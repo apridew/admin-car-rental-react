28404af108b8a98d1bf9b22dacdc0f2620ccbec0
apridew <dewiapriyantiputra@gmail.com>
2023-12-10 05:47:16 +0700

    add button search components

---
+++ src/components/ButtonSearch/index.jsx
@@ -0,0 +1,10 @@
+import './style.css';
+const ButtonSearch = ({ value }) => {
+  return (
+    <>
+      <button className="btn-search">{value}</button>
+    </>
+  );
+};
+
+export default ButtonSearch;
