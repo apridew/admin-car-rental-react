bc189f2bd9b1456145140368c3df9c821dcfa8c9
apridew <dewiapriyantiputra@gmail.com>
2023-12-10 05:48:13 +0700

    add card car components

---
+++ src/components/CardCar/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './style.css';
+
+const CardCar = ({ img, name, price, capacity, time }) => {
+  return (
+    <>
+      <div id="card-car" className="card">
+        <img src={img} alt={name} />
+        <p>{name}</p>
+        <p>{price}/hari</p>
+        <p>
+          <i className="bi bi-people"></i>
+          {capacity}
+        </p>
+        <p>
+          <i className="bi bi-clock"></i>
+          {time}
+        </p>
+        <div className="button-bottom d-flex gap-3">
+          <button>
+            <i className="bi bi-trash"></i> Delete
+          </button>
+          <button>
+            <i className="bi bi-pencil-square"></i> Edit
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CardCar;
